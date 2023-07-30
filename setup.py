73b5d964695d8652becbe51924593d6447420cd9
Marcos <marcos.esteban.soto@gmail.com>
2023-07-30 18:46:32 -0300

    gas data array order

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="modbus_toolbox",
-    version="0.1.7",
+    version="0.1.8",
     author="Marcos E Soto",
     author_email="marcos.esteban.soto@gmail.com",
     description="Modbus Toolbox",
